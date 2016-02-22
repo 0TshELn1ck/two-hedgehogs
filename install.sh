d33d14a427928efda235418188e28e466a83e2b1
Mihail Pronyuk <mishaprk@gmail.com>
2016-02-23 00:15:19 +0200

    add install script

--- install.sh
+++ install.sh
@@ -5,9 +5,8 @@ echo "Project Two hedgehogs "
 echo "what you wanna do?"
 echo "1 - install package"
 echo "2 - create base, schema and load fixtures"
-echo "3 - run tests"
-echo "4 - clear cache"
-echo "5 - drop database"
+echo "3 - clear cache"
+echo "4 - drop database"
 echo "0 - exit"
 echo;
 
@@ -23,15 +22,11 @@ case "$key" in
    "2" ) 
       ./app/console doctrine:database:create
       ./app/console doctrine:schema:update --force   
-#      ./app/console h:d:f:l -n
    ;;
    "3" ) 
-#     ./phpunit -c app
-   ;;
-   "4" ) 
      ./app/console cache:clear  
    ;;
-   "5" ) 
+   "4" ) 
      ./app/console doctrine:database:drop --force 
    ;;
    "0" ) ;;
