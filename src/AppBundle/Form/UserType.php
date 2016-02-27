fe94e338a940cf4b5eb36f5d665f385c3f71d728
0TshEL_n1ck <plysvladimir@gmail.com>
2016-02-27 13:35:14 +0200

    created registration form and added in template

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
@@ -15,11 +14,29 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class, array(
+                'attr' => array(
+                    'placeholder' => 'Почтова адреса',
+                    'class' => 'form-control'
+                ),
+                'label' => false
+            ))
             ->add('password', RepeatedType::class, array(
                     'type' => PasswordType::class,
-                    'first_options' => array('label' => 'Password'),
-                    'second_options' => array('label' => 'Repeat Password'),
+                    'first_options' => array(
+                        'attr' => array(
+                            'placeholder' => 'Пароль',
+                            'class' => 'form-control'
+                        ),
+                        'label' => false
+                    ),
+                    'second_options' => array(
+                        'attr' => array(
+                            'placeholder' => 'Повторіть пароль',
+                            'class' => 'form-control'
+                        ),
+                        'label' => false
+                    ),
                 )
             );
     }
@@ -31,7 +48,7 @@ class UserType extends AbstractType
         ));
     }
 
-    public function getName()
+    public function getBlockPrefix()
     {
         return 'app_bundle_user_type';
     }
