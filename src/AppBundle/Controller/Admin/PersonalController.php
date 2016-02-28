ff1cd79005d5f376840823a3ffc63f88c6c107f9
Plys Vladimir <plysvladimir@gmail.com>
2016-02-28 20:03:19 +0200

    added controller and template Personal

---
+++ src/AppBundle/Controller/Admin/PersonalController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PersonalController extends Controller
+{
+    /**
+     * @return array
+     * @Route("/personal", name="admin_personal")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+}
