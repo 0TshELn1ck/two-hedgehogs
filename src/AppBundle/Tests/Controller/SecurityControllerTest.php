0aa0e15f2abdb9ad738d13025461fade7ae6b010
Plys Vladimir <plysvladimir@gmail.com>
2016-03-01 22:20:18 +0200

    add 1 test and fix personal route

---
+++ src/AppBundle/Tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+    public function testLogin()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+}
