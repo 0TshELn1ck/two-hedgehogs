e68966c975acf646541de967660462dc7cb9f549
Plys Vladimir <plysvladimir@gmail.com>
2016-03-04 21:26:49 +0200

    added user in template + some refactor

--- src/AppBundle/Controller/Admin/UserController.php
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -1,16 +1,17 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Admin;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class UserController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     * @Template("default/index.html.twig")
+     * @return array
+     * @Route("/admin/user", name="admin_user")
+     * @Template()
      */
     public function indexAction()
     {
