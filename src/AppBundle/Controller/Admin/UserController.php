96f588b16d4122fb5275aea9632d0d6f214a10e6
Plys Vladimir <plysvladimir@gmail.com>
2016-03-06 11:00:00 +0200

    added some user action and create index template

--- src/AppBundle/Controller/Admin/UserController.php
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller\Admin;
 
+use AppBundle\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -26,6 +29,56 @@ class UserController extends Controller
         $paginator = $this->get('knp_paginator');
         $pagination = $paginator->paginate($user, $request->query->getInt('page', 1), 10);
 
-        return ['user' => $pagination];
+        $deleteForm = [];
+        foreach ($user as $entity) {
+            $deleteForm[$entity->getId()] = $this->createDeleteForm($entity)->createView();
+        }
+
+        return ['user' => $pagination, 'deleteForm' => $deleteForm];
+    }
+
+    /**
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/admin/user/delete/{id}", name="admin_user_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction($id)
+    {
+        if ($id) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AppBundle:User')->findOneBy(
+                array(
+                    'id' => $id,
+                )
+            );
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find User entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_user_index'));
+    }
+
+    /**
+     * Creates a form to delete a User entity.
+     * @param User $user The User entity
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(User $user)
+    {
+
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_user_delete', array('id' => $user->getId())))
+            ->setMethod('DELETE')
+            ->add('submit', SubmitType::class, [
+                'label' => ' Видалити',
+                'attr' => ['class' => 'btn btn-xs btn-danger ace-icon fa fa-trash-o bigger-120']
+            ])
+            ->getForm();
     }
 }
