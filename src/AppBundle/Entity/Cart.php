c3ce9c52c72a936fe96077cbc286a18d4206aafa
DenisRupp <rupp1993@gmail.com>
2016-03-09 22:00:30 +0200

    Create Cart entity and relations

---
+++ src/AppBundle/Entity/Cart.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Cart
+ *
+ * @ORM\Table(name="cart")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CartRepository")
+ */
+class Cart
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ip", type="string", length=15, nullable=true, unique=true)
+     */
+    private $ip;
+
+    /**
+     * @ORM\OneToOne(targetEntity="AppBundle\Entity\User", inversedBy="Cart")
+     */
+    private $user;
+
+    /**
+     * @var \DateTime $createdAt
+     *
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="AppBundle\Entity\Dish", inversedBy="Cart")
+     */
+    private $dishes;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ip
+     *
+     * @param string $ip
+     *
+     * @return Cart
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    /**
+     * Get ip
+     *
+     * @return string
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Cart
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return Cart
+     */
+    public function setUser(\AppBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->dishes = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add dish
+     *
+     * @param \AppBundle\Entity\Dish $dish
+     *
+     * @return Cart
+     */
+    public function addDish(\AppBundle\Entity\Dish $dish)
+    {
+        $this->dishes[] = $dish;
+
+        return $this;
+    }
+
+    /**
+     * Remove dish
+     *
+     * @param \AppBundle\Entity\Dish $dish
+     */
+    public function removeDish(\AppBundle\Entity\Dish $dish)
+    {
+        $this->dishes->removeElement($dish);
+    }
+
+    /**
+     * Get dishes
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getDishes()
+    {
+        return $this->dishes;
+    }
+}
