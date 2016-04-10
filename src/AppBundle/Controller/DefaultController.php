9538b310bc36f6261992ea997ffce61f3f47791f
DenisRupp <rupp1993@gmail.com>
2016-04-10 14:58:45 +0300

    edit templates and add popular dishes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,9 +20,12 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $dishList = $em->getRepository('AppBundle:Dish')->getDishes();
         $dishPictList = $this->get('gallery.dishes')->randomPictures(5);
+        $popularDish = $em->getRepository('AppBundle:Dish')->getPopularDish();
         
         return [
-            'dishList' => $dishList, 'dishPictList' => $dishPictList
+            'dishList' => $dishList, 
+            'dishPictList' => $dishPictList,
+            'popularDish'=>$popularDish
         ];
     }
 }
