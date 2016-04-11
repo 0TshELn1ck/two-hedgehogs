d3bffba5f056d9d760509a513c104d802fbb06d3
DenisRupp <rupp1993@gmail.com>
2016-04-11 23:12:03 +0300

    add ajax for feedback modal in admin

--- src/AppBundle/Controller/Admin/FeedbackController.php
+++ src/AppBundle/Controller/Admin/FeedbackController.php
@@ -4,18 +4,21 @@ namespace AppBundle\Controller\Admin;
 
 
 use AppBundle\Entity\Feedback;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 
 /**
  * Class FeedbackController
- * @Route("/admin")
+ * @Route("/admin/feedback")
  */
 class FeedbackController extends Controller
 {
     /**
-     * @Route("/feedback", name="admin_feedback_index")
+     * @Route("/", name="admin_feedback_index")
      */
     public function indexAction()
     {
@@ -26,7 +29,7 @@ class FeedbackController extends Controller
 
     /**
      * @param Feedback $feedback
-     * @Route("/feedback/{id}", name="admin_feedback_show", requirements={"id": "\d+"})
+     * @Route("/{id}", name="admin_feedback_show", requirements={"id": "\d+"})
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function showAction(Feedback $feedback)
@@ -38,5 +41,34 @@ class FeedbackController extends Controller
         $em->flush();
 
         return $this->render('@App/Admin/Feedback/show.html.twig', ['feedback' => $feedback]);
+    }
+
+    /**
+     * Count of new feedbacks
+     *
+     * @Route("/count", name="get_feed_count")
+     */
+    public function getFeedCountAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $feeds = $em->getRepository('AppBundle:Feedback')->findBy(array('status'=>false));
+
+        return new Response(count($feeds));
+    }
+
+    /**
+     * Get new feedbacks by ajax
+     *
+     * @Route("/new", name="get_new_feedback")
+     * @Method("POST")
+     * @Template("AppBundle:Admin/Modals:feedback.html.twig")
+     */
+    public function getNewFeedbacksAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $feedbacks = $em->getRepository('AppBundle:Feedback')
+            ->findBy(array('status'=>false));
+
+        return ['feedbacks' => $feedbacks];
     }
 }
